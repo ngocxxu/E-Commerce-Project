d525320b724b9261fe9f35fd2adbfe7252bc1302
ngocxxu <ngocquach43@gmail.com>
2021-11-29 22:02:03 +0700

    backup and get data commerce.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// import Navbar from "./components/Navbar/Navbar";
+// import Products from './components/Products/Product';
+import { Products, Navbar } from "./components";
+import {commerce} from './lib/commerce';
 
 const App = () => {
+  const [products, setProducts] = useState([])
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list(); //return promises
+    setProducts(data);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
     <div>
-      123
+      <Navbar />
+      <Products products = {products} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
